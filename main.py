7d9e8e3137dac49da981047e8d03bde88e6ba116
Olve Odegard <olve.odegard@gmail.com>
2020-11-25 13:01:49 +0000

    Updating address to use and finally being able to get tokens and check the balance of the address on the testnet

--- main.py
+++ main.py
@@ -1,19 +1,18 @@
+import sys, os
 from iota import Iota
 from iota import Address
 
-iotaNode = "https://node1.rosipay.net:443"
+# Connect to node
+api = Iota("https://nodes.devnet.iota.org:443", testnet = True)
 
-"""
-api = Iota(iotaNode, "")
-address = [Address(b'NYZBHOVSMDWWABXSACAJTTWJOQRPVVAWLBSFQVSJSWWBJJLLSQKNZFC9XCRPQSVFQZPBJCJRANNPVMMEZQJRQSVVGZ')]
-gb_result = api.get_balances(address)
-balance = gb_result['balances']
-print("Balance of some random address: " + str(balance[0]))
-"""
+# Read out address to use from file
+address = []
+with open(os.path.join(sys.path[0], "addresses_testnet"), "r") as f:
+    address.append(f.read())
 
-seed = 'RWCX9QOBHEKGIYZHIZOVSIRHKFF9QVPEIQBSQKHXMFODEQHEWPWCHDZAXHJJNQWIJINYVJZVTGBHSDY9S'
-api = Iota("https://node1.rosipay.net:443", seed, testnet = True)
-address = api.get_new_addresses(index=0, count=1, security_level=2)['addresses'][0]
-is_spent = api.were_addresses_spent_from([address])['states'][0]
-print(address)
-print(is_spent)
+def getbalance():
+    print("Checking balance")
+    return api.get_balances(address)["balances"]
+    
+balance = getbalance()
+print(balance)
